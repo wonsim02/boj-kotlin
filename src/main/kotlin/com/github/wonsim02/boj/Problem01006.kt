9c85101e2935890b026a928e2b4cd8c9395f2cb8
wonsim02 <wonsim0718@gmail.com>
2024-02-18 00:19:50 +0900

    make dynamic cases const values

--- src/main/kotlin/com/github/wonsim02/boj/Problem01006.kt
+++ src/main/kotlin/com/github/wonsim02/boj/Problem01006.kt
@@ -4,10 +4,6 @@ import java.util.Scanner
 
 /**
  * [1006번 문제](https://www.acmicpc.net/problem/1006)에 대한 답안. ([제출된 답안](http://boj.kr/84490a63740d44b78510b5362e49081c))
- * 1. `i`, `n+i` 둘 다 사용 가능
- * 1. `i`만 사용 가능
- * 1. `n+i`만 사용 가능
- * 1. `i`, `n+i` 둘 다 사용 불가능
  */
 object Problem01006 {
 
@@ -16,6 +12,45 @@ object Problem01006 {
 
     private const val INNER = 0
     private const val OUTER = 1
+
+    /**
+     * `1` & `n` 구역도, `n+1` & `2n` 구역도 한 팀으로 묶이지 않은 경우
+     */
+    private const val FIRST_CASE0 = 0
+    /**
+     * `n+1` & `2n` 구역이 한 팀으로 묶인 경우
+     */
+    private const val FIRST_CASE1 = 1
+    /**
+     * `1` & `n` 구역이 한 팀으로 묶인 경우
+     */
+    private const val FIRST_CASE2 = 2
+    /**
+     * `1` & `n` 구역도, `n+1` & `2n` 구역도 한 팀으로 묶인 경우
+     */
+    private const val FIRST_CASE3 = 3
+
+    /**
+     * `i` 및 `n+i` 구역에 대해 `i`구역도, `n+i`구역도 다른 구역과 묶이지 않은 경우
+     */
+    private const val CURRENT_CASE0 = 0
+    /**
+     * `i` 및 `n+i` 구역에 대해 `n+i` 구역이 타 구역(`n+i-1`)과 묶인 경우
+     */
+    private const val CURRENT_CASE1 = 1
+    /**
+     * `i` 및 `n+i` 구역에 대해 `i` 구역이 타 구역(`i-1`)과 묶인 경우
+     */
+    private const val CURRENT_CASE2 = 2
+    /**
+     * `i` 및 `n+i` 구역에 대해 `i` 구역과 `n+i` 구역 둘 다 타 구역과 묶인 경우. 다음 두 가지 경우가 가능하다.
+     * - `i` & `n+i`
+     * - `i-1` & `i`, `n+i-1` & `n+i`
+     */
+    private const val CURRENT_CASE3 = 3
+
+    private val firstCases = listOf(FIRST_CASE0, FIRST_CASE1, FIRST_CASE2, FIRST_CASE3)
+    private val currentCases = listOf(CURRENT_CASE0, CURRENT_CASE1, CURRENT_CASE2, CURRENT_CASE3)
 
     private val scanner = Scanner(System.`in`)
     private val numEnemiesInArea: Array<Array<Short>> = Array(2) { Array(MAX_N) { 0 } }
@@ -96,15 +131,15 @@ object Problem01006 {
             flag1 = numEnemiesInArea[OUTER][0] + numEnemiesInArea[OUTER][n-1] <= w
             flag2 = numEnemiesInArea[INNER][0] + numEnemiesInArea[OUTER][0] <= w
 
-            for (index0 in 0 until 4) {
-                for (index1 in 0 until 4) {
+            for (index0 in firstCases) {
+                for (index1 in currentCases) {
                     setMinNumbers(0, index0, index1) {
                         when {
-                            index0 == 0 && index1 == 3 && flag2 -> 1
-                            index0 == 0 && index1 == 0 -> 2
-                            index0 == 1 && index1 == 1 && flag1 -> 2
-                            index0 == 2 && index1 == 2 && flag0 -> 2
-                            index0 == 3 && index1 == 3 && flag0 && flag1 -> 2
+                            index0 == FIRST_CASE0 && index1 == CURRENT_CASE3 && flag2 -> 1
+                            index0 == FIRST_CASE0 && index1 == CURRENT_CASE0 -> 2
+                            index0 == FIRST_CASE1 && index1 == CURRENT_CASE1 && flag1 -> 2
+                            index0 == FIRST_CASE2 && index1 == CURRENT_CASE2 && flag0 -> 2
+                            index0 == FIRST_CASE3 && index1 == CURRENT_CASE3 && flag0 && flag1 -> 2
                             else -> INFINITY
                         }
                     }
@@ -116,14 +151,14 @@ object Problem01006 {
                 flag1 = numEnemiesInArea[OUTER][i-1] + numEnemiesInArea[OUTER][i] <= w
                 flag2 = numEnemiesInArea[INNER][i] + numEnemiesInArea[OUTER][i] <= w
 
-                for (index0 in 0 until 4) {
-                    prev0 = getMinNumbers(i-1, index0, 0)
-                    prev1 = getMinNumbers(i-1, index0, 1)
-                    prev2 = getMinNumbers(i-1, index0, 2)
-                    prev3 = getMinNumbers(i-1, index0, 3)
+                for (index0 in firstCases) {
+                    prev0 = getMinNumbers(i-1, index0, CURRENT_CASE0)
+                    prev1 = getMinNumbers(i-1, index0, CURRENT_CASE1)
+                    prev2 = getMinNumbers(i-1, index0, CURRENT_CASE2)
+                    prev3 = getMinNumbers(i-1, index0, CURRENT_CASE3)
 
                     m = minOf(prev0, prev1, prev2, prev3)
-                    setMinNumbers(i, index0, 0) {
+                    setMinNumbers(i, index0, CURRENT_CASE0) {
                         if (m != INFINITY) {
                             (m + 2).toShort()
                         } else {
@@ -132,7 +167,7 @@ object Problem01006 {
                     }
 
                     m02 = minOf(prev0, prev2)
-                    setMinNumbers(i, index0, 1) {
+                    setMinNumbers(i, index0, CURRENT_CASE1) {
                         if (m02 != INFINITY && flag1) {
                             (m02 + 1).toShort()
                         } else {
@@ -141,7 +176,7 @@ object Problem01006 {
                     }
 
                     m01 = minOf(prev0, prev1)
-                    setMinNumbers(i, index0, 2) {
+                    setMinNumbers(i, index0, CURRENT_CASE2) {
                         if (m01 != INFINITY && flag0) {
                             (m01 + 1).toShort()
                         } else {
@@ -149,7 +184,7 @@ object Problem01006 {
                         }
                     }
 
-                    setMinNumbers(i, index0, 3) {
+                    setMinNumbers(i, index0, CURRENT_CASE3) {
                         minOf(
                             if (m != INFINITY && flag2) {
                                 (m + 1).toShort()
@@ -171,17 +206,17 @@ object Problem01006 {
             flag2 = numEnemiesInArea[INNER][n-1] + numEnemiesInArea[OUTER][n-1] <= w
 
             result = INFINITY
-            for (index0 in 0 until 4) {
-                prev0 = getMinNumbers(n-2, index0, 0)
-                prev1 = getMinNumbers(n-2, index0, 1)
-                prev2 = getMinNumbers(n-2, index0, 2)
-                prev3 = getMinNumbers(n-2, index0, 3)
+            for (index0 in firstCases) {
+                prev0 = getMinNumbers(n-2, index0, CURRENT_CASE0)
+                prev1 = getMinNumbers(n-2, index0, CURRENT_CASE1)
+                prev2 = getMinNumbers(n-2, index0, CURRENT_CASE2)
+                prev3 = getMinNumbers(n-2, index0, CURRENT_CASE3)
 
                 m = minOf(prev0, prev1, prev2, prev3)
                 m01 = minOf(prev0, prev1)
                 m02 = minOf(prev0, prev2)
 
-                if (index0 == 0) {
+                if (index0 == FIRST_CASE0) {
                     result = minOf(result, (m + 2).toShort())
                     if (m != INFINITY && flag2) {
                         result = minOf(result, (m + 1).toShort())
@@ -195,12 +230,12 @@ object Problem01006 {
                     if (prev0 != INFINITY && flag0 && flag1) {
                         result = minOf(result, prev0)
                     }
-                } else if (index0 == 1) {
+                } else if (index0 == FIRST_CASE1) {
                     result = minOf(result, (m + 1).toShort())
                     if (m01 != INFINITY && flag0) {
                         result = minOf(result, m01)
                     }
-                } else if (index0 == 2) {
+                } else if (index0 == FIRST_CASE2) {
                     result = minOf(result, (m + 1).toShort())
                     if (m02 != INFINITY && flag1) {
                         result = minOf(result, m02)
